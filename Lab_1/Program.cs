128895e601de22bbe7987b583080e176d3935636
Sergey Bronskiy <s.s.bronskiy@gmail.com>
2017-03-13 23:09:41 +0300

    Hello world program

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab_1
 {
@@ -10,6 +6,7 @@ namespace Lab_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
         }
     }
 }
