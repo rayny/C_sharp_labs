f9a92138f6642f49015f7524419e89b7d3d4284a
Sergey Bronskiy <s.s.bronskiy@gmail.com>
2017-03-14 21:00:52 +0300

    performed task 5: program commented

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿/*Lab_1, Author: Sergey Bronskiy*/
+
+using System; //System namespace is used here to call class Console
 
 namespace Lab_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //method Main is required here, it is called when program starts
+            //the Main method does not return anything in this case
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!"); //method WriteLine from Console class is used to output string to stdout
         }
     }
 }
