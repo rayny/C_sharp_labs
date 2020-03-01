3d6f7d912a67bd4892545f7645257fd41485ea3f
Sergey Bronskiy <s.s.bronskiy@gmail.com>
2020-03-01 13:41:56 +0300

    Lab2_task5

---
+++ Lab_2/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            string s = args[0];
+            int i = int.Parse(s);
+            Console.WriteLine(i);
+        }
+    }
+}
