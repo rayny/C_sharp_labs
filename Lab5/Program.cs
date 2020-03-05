3de52ba04c31e865fe4722913a1dbcafa4d4dbef
Sergey Bronskiy <sbronskiy.mtt@gmail.com>
2020-03-05 12:18:37 +0300

    Lab_5 task_2

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab5
 {
@@ -18,6 +19,35 @@ namespace Lab5
         public void PrintToConsole()
         {
             Console.WriteLine("X: {0}; Y: {1}", X, Y);
+        }
+    }
+
+    public class MyStack<T>
+    {
+        private List<T> list;
+
+        public void Add(T x)
+        {
+            if (list == null)
+            {
+                list = new List<T>{x};
+            }
+            else
+            {
+                list.Add(x);
+            }
+            
+        }
+
+        public T Pop()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            T a = list[list.Count-1];
+            list.RemoveAt(list.Count - 1);
+            return a;
         }
     }
 }
