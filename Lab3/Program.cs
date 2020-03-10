f73ae56d705f89887db382dcd6c82fc645d92762
Sergey Bronskiy <s.s.bronskiy@gmail.com>
2020-03-10 21:01:56 +0300

    Lab3_task1_dummy

---
+++ Lab3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day[, ] weeks = new Day[10, 7];
+            for (int i = 0; i < 10; i++ )
+            {
+                for (int j = 0; j < 7; j++)
+                {
+
+                }
+            }
+        }
+    }
+
+    public class Day
+    {
+        enum ISworkingDay { Working, Weekend}
+        ISworkingDay workingDay { get; set; }
+        List<Purchase> purchases { get; set; }
+    }
+
+    public class Purchase
+    {
+        int amount { get; set; }
+        enum IScash { Cash, Card}
+        IScash cash { get; set; }
+    }
+}
